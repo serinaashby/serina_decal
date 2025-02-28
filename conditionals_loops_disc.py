da57139ec32c45641e6cafbe886abd7f894aa591
Serina Nadine <serina@wifi-10-40-96-220.wifi.berkeley.edu>
2025-02-27 16:59:13 -0800

    This Homework 2 is complete

---
+++ conditionals_loops_disc.py
@@ -0,0 +1,46 @@
+# Conditionals & Loops Discussion ! :)
+
+# Quick refresher: 
+
+# This is the syntax of a for-loop:
+# for variable in range(start, stop, step):
+    # Code block to be executed
+
+# This is the syntax of a while-loop:
+# while condition == true:
+    # Code block to be executed
+
+# Note: It is normal to forget the syntax! That is why google is your bff and you just need to google "for loop syntax" or "while loop syntax"
+# There are also tons of resources. I usually just go to images to quickly check the syntax. 
+# Remember to cite where you got your code from if you are borrowing a block of code. 
+
+
+
+
+
+'''Problem 1: Your turn!
+Print i 10 times using both a for and while loop.'''
+#YOUR CODE HERE
+
+
+
+'''Problem 2: Conditionals
+Think of all the ways you can compare a number, x, with a value, 10. Write a function that will take in a value of x and compare it with 10.'''
+#YOUR CODE HERE
+
+
+
+'''Problem 3: Range()
+Range is a cool function but there are a lot of parts to it so let's disect it!
+What happens if the range is from (5, 5)? How about from (10, 6)?
+Try different step sizes: (10, 6, -1), (1, 10, 2). Write down all your observations
+and answer the following questions.'''
+#YOUR CODE HERE
+
+
+
+'''Problem 4: Listness
+How do you loop through elements in a list using a for loop? '''
+#YOUR CODE HERE
+
+
