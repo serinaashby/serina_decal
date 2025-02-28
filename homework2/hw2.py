58811a567a4ee41746e61c7b43ac7769e7c2fd71
serinaashby <serinaashby@berkeley.edu>
2025-02-27 16:44:03 -0800

    Homework #2 Completed

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,32 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# A: cd ../..
+
 # 2) What does ~/ mean?
+# A: shortcut to home directory
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# A: An absolute path has the complete path where a relative path is the path from your current working directory 
+
 # 4) What command would bring you back to your home directory?
+# A: cd
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# A: It would remove all the files in your current directory
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# A: git add can add whatever you tell it to add to the location you specify, 
+# A: git commit takes a snapshot of the changes made to save to the git repository,
+# A: git push uploads the committed changes from your local repository to a remote repository
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# A: git status shows all the files added and shows what files are staged for the next commit, what's been added or modified, and how to unstage them
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# A: I haven't run into too many errors in this class, but for my Thermodynamics class, I have an autograder on my python assignments
+# A: and if my variables don't match exactly to the autograder, i get marked incorrect even when my code is right!
+
 # 9) How does cloning a repository differ from pulling from a repository?
+# A: Cloning creates a complete copy on your local computer where pulling pulls changes and merges them with your local repository
+
 # 10) Tell me a fun fact!
+# A: Kendrick Lamar's album won a Pulitzer prize 
